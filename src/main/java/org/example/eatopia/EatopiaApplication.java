3fe12fabc4bda50edde5553e82cf2ee537deb6c3
seongmin2223 <khyuno9@naver.com>
2025-10-16 09:59:27 +0900

    chore/basic:init setting

---
+++ src/main/java/org/example/eatopia/EatopiaApplication.java
@@ -0,0 +1,13 @@
+package org.example.eatopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatopiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatopiaApplication.class, args);
+    }
+
+}
