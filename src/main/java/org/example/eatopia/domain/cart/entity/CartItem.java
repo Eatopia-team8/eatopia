7db9a3969503f80fd0f9368b8834c7bd706b6a49
Hyjpark <hyej042@gmail.com>
2025-10-16 15:40:13 +0900

    feat/cart: Cart와 CartItem 엔티티 기본 필드 추가

---
+++ src/main/java/org/example/eatopia/domain/cart/entity/CartItem.java
@@ -0,0 +1,34 @@
+package org.example.eatopia.domain.cart.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.eatopia.common.core.entity.BaseEntity;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CartItem extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long cartId;
+
+    @Column(nullable = false)
+    private Long productId;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(precision = 10, scale = 0, nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private boolean isSelected;
+}
