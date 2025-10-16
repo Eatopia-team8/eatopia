84daef106e44d89be62d618ea8049ed23aa062e8
Hyjpark <hyej042@gmail.com>
2025-10-16 16:54:57 +0900

    refactor/cart: base API 경로를 YML로 관리하도록 변경

--- src/main/java/org/example/eatopia/domain/cart/controller/CartController.java
+++ src/main/java/org/example/eatopia/domain/cart/controller/CartController.java
@@ -3,12 +3,10 @@ package org.example.eatopia.domain.cart.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.eatopia.domain.cart.service.command.CartCommandService;
 import org.example.eatopia.domain.cart.service.query.CartQueryService;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/eatopia")
 public class CartController {
 
     private final CartCommandService cartCommandService;
