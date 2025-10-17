e3f3975cb5d044998d2566d84f2a49ba5205fdfd
Hyjpark <hyej042@gmail.com>
2025-10-17 15:38:57 +0900

    refactor/cart: 하드코딩 userId를 @AuthenticationPrincipal로 변경

--- src/main/java/org/example/eatopia/domain/cart/controller/CartController.java
+++ src/main/java/org/example/eatopia/domain/cart/controller/CartController.java
@@ -2,11 +2,13 @@ package org.example.eatopia.domain.cart.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.eatopia.common.core.dto.Response;
+import org.example.eatopia.domain.auth.dto.AuthUser;
 import org.example.eatopia.domain.cart.dto.request.CartCreateRequest;
 import org.example.eatopia.domain.cart.dto.response.CartCreateResponse;
 import org.example.eatopia.domain.cart.service.command.CartCommandService;
 import org.example.eatopia.domain.cart.service.query.CartQueryService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +21,10 @@ public class CartController {
     private final CartQueryService cartQueryService;
 
     @PostMapping("/v1/carts/items")
-    public ResponseEntity<Response<CartCreateResponse>> createCartItem(@RequestBody CartCreateRequest request) {
+    public ResponseEntity<Response<CartCreateResponse>> createCartItem(@RequestBody CartCreateRequest request,
+                                                                       @AuthenticationPrincipal AuthUser authUser) {
 
-        // 인증 전 userId 하드코딩
-        Long userId = 1L;
-        CartCreateResponse createCartItem = cartCommandService.createCartItem(userId, request);
+        CartCreateResponse createCartItem = cartCommandService.createCartItem(authUser.getId(), request);
         return ResponseEntity.ok(Response.success(createCartItem));
     }
 }
