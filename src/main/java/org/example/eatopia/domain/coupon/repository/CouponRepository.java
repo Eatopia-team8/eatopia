ec81e13fe1160c5b8892b2a8334583907bbd18bc
MinHyeongK <syjjmh1@naver.com>
2025-10-17 17:02:00 +0900

    feat/coupon: 쿠폰 상세 조회 api 구현

--- src/main/java/org/example/eatopia/domain/coupon/repository/CouponRepository.java
+++ src/main/java/org/example/eatopia/domain/coupon/repository/CouponRepository.java
@@ -3,7 +3,11 @@ package org.example.eatopia.domain.coupon.repository;
 import org.example.eatopia.domain.coupon.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
     boolean existsByCode(String code);
+
+    Optional<Coupon> findCouponById(Long id);
 }
