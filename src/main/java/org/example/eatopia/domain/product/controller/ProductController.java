584d1cc28131e10c4e0a3bae904e7d1fe8432849
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-10-17 22:44:52 +0900

    style/product-create: 안 쓰는 주입 제거

--- src/main/java/org/example/eatopia/domain/product/controller/ProductController.java
+++ src/main/java/org/example/eatopia/domain/product/controller/ProductController.java
@@ -6,7 +6,6 @@ import org.example.eatopia.common.core.dto.Response;
 import org.example.eatopia.domain.product.dto.request.ProductCreateRequest;
 import org.example.eatopia.domain.product.dto.response.ProductResponse;
 import org.example.eatopia.domain.product.service.command.ProductCommandService;
-import org.example.eatopia.domain.product.service.query.ProductQueryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductCommandService productCommandService;
-    private final ProductQueryService productQueryService;
 
     // 상품 등록 (판매자)
     @PreAuthorize("hasRole('ROLE_SELLER')")
