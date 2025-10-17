1891ff97416147d2f0598c12449e083b98ffdf4e
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-10-17 16:04:55 +0900

    feat/category-create: 카테고리 생성 (빠진 커밋 파일)

--- src/main/java/org/example/eatopia/domain/category/repository/CategoryRepository.java
+++ src/main/java/org/example/eatopia/domain/category/repository/CategoryRepository.java
@@ -4,4 +4,6 @@ import org.example.eatopia.domain.category.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    boolean existsByName(String name);
 }
