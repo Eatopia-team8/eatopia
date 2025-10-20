86ff9f5f129895f77031807fa22d72d6d87879a2
Hyjpark <hyej042@gmail.com>
2025-10-20 16:36:36 +0900

    feat/cart: enum 타입 소문자 허용

--- src/main/java/org/example/eatopia/domain/cart/enums/QuantityChangeType.java
+++ src/main/java/org/example/eatopia/domain/cart/enums/QuantityChangeType.java
@@ -1,11 +1,16 @@
 package org.example.eatopia.domain.cart.enums;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum QuantityChangeType {
+
+    @JsonProperty("increment")
     INCREMENT, // 증가
+
+    @JsonProperty("decrement")
     DECREMENT // 감소
 }
