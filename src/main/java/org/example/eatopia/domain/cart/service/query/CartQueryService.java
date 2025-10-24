7b96e584ad6ff2c0e1785ef48e0cbd49ea2f5916
Hyjpark <hyej042@gmail.com>
2025-10-24 10:30:43 +0900

    feat/cart: 선택된 장바구니 상품 조회 서비스 로직 추가

--- src/main/java/org/example/eatopia/domain/cart/service/query/CartQueryService.java
+++ src/main/java/org/example/eatopia/domain/cart/service/query/CartQueryService.java
@@ -2,10 +2,15 @@ package org.example.eatopia.domain.cart.service.query;
 
 import org.example.eatopia.domain.cart.dto.response.CartResponse;
 import org.example.eatopia.domain.cart.entity.Cart;
+import org.example.eatopia.domain.cart.entity.CartItem;
+
+import java.util.List;
 
 public interface CartQueryService {
 
     CartResponse getCartByUser(Long userId);
 
     Cart getCart(Long userId);
+
+    List<CartItem> getSelectedCartItems(Long userId);
 }
