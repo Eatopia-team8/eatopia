a3449a38057469d22f7e8315c46fbeaf2e59450b
Hyjpark <hyej042@gmail.com>
2025-10-24 21:20:55 +0900

    feat/cart: Review 엔티티 작성

---
+++ src/main/java/org/example/eatopia/domain/review/entity/Review.java
@@ -0,0 +1,74 @@
+package org.example.eatopia.domain.review.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.eatopia.common.core.entity.SoftDeleteEntity;
+import org.example.eatopia.domain.product.entity.Product;
+import org.example.eatopia.domain.review.enums.ReviewStatus;
+import org.example.eatopia.domain.user.entity.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review extends SoftDeleteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false, columnDefinition = "TINYINT CHECK (rating BETWEEN 1 AND 5)")
+    private Integer rating;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ReviewStatus status;
+
+    private LocalDateTime reportedAt;
+
+    private int reportCount;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Review(User user,
+                   Product product,
+                   String content,
+                   Integer rating,
+                   ReviewStatus status,
+                   LocalDateTime reportedAt,
+                   int reportCount) {
+        this.user = user;
+        this.product = product;
+        this.content = content;
+        this.rating = rating;
+        this.status = status;
+        this.reportedAt = reportedAt;
+        this.reportCount = reportCount;
+    }
+
+    public static Review create(User user, Product product, String content, Integer rating, ReviewStatus status) {
+        return Review.builder()
+                .user(user)
+                .product(product)
+                .content(content)
+                .rating(rating)
+                .status(ReviewStatus.ACTIVE)
+                .reportCount(0)
+                .build();
+    }
+
+}
