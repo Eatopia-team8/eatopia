414c9ac5589aba4544cd7111c687c8ee1328ac13
Hyjpark <hyej042@gmail.com>
2025-10-27 16:54:24 +0900

    feat/review: 리뷰 생성 로직 개선

--- src/main/java/org/example/eatopia/domain/review/service/command/ReviewCommandServiceImpl.java
+++ src/main/java/org/example/eatopia/domain/review/service/command/ReviewCommandServiceImpl.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.example.eatopia.common.core.exception.GlobalException;
 import org.example.eatopia.domain.order.entity.OrderDetail;
 import org.example.eatopia.domain.order.service.query.OrderQueryService;
-import org.example.eatopia.domain.product.entity.Product;
 import org.example.eatopia.domain.product.service.query.ProductQueryService;
 import org.example.eatopia.domain.review.dto.request.ReviewRequest;
 import org.example.eatopia.domain.review.dto.response.ReviewResponse;
@@ -12,7 +11,6 @@ import org.example.eatopia.domain.review.entity.Review;
 import org.example.eatopia.domain.review.exception.ReviewErrorCode;
 import org.example.eatopia.domain.review.repository.ReviewReportRepository;
 import org.example.eatopia.domain.review.repository.ReviewRepository;
-import org.example.eatopia.domain.user.entity.User;
 import org.example.eatopia.domain.user.service.query.UserQueryService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -40,10 +38,9 @@ public class ReviewCommandServiceImpl implements ReviewCommandService {
         // 주문 내역 있는지 확인
         OrderDetail orderDetail = orderQueryService.getOrderDetailbByUserId(orderDetailId, userId);
 
-        User user = userQueryService.getUserEntityById(userId);
-        Product product = productQueryService.getProductOrElseThrow(orderDetail.getProduct().getId());
         Review review = Review.create(
-                user, product,
+                orderDetail.getOrder().getUser(),
+                orderDetail.getProduct(),
                 orderDetail,
                 request.content(),
                 request.rating()
