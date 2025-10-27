7703bfee7b51818abac58fe41f1de7ec0c66c8d9
dbdnsdb123 <dbdnsdb123@naver.com>
2025-10-27 11:59:39 +0900

    feat/refund: refund CRUD 추가

---
+++ src/main/java/org/example/eatopia/domain/refund/service/command/RefundCommandServiceImpl.java
@@ -0,0 +1,92 @@
+package org.example.eatopia.domain.refund.service.command;
+
+import lombok.RequiredArgsConstructor;
+import org.example.eatopia.domain.order.entity.OrderDetail;
+import org.example.eatopia.domain.order.service.query.OrderDetailQueryService;
+import org.example.eatopia.domain.payment.entity.Payment;
+import org.example.eatopia.domain.payment.service.query.PaymentQueryService;
+import org.example.eatopia.domain.product.service.command.ProductCommandService;
+import org.example.eatopia.domain.refund.dto.event.RefundSuccessEvent;
+import org.example.eatopia.domain.refund.dto.request.RefundCreateRequest;
+import org.example.eatopia.domain.refund.dto.response.RefundResponse;
+import org.example.eatopia.domain.refund.entity.Refund;
+import org.example.eatopia.domain.refund.enums.RefundStatus;
+import org.example.eatopia.domain.refund.exception.RefundErrorCode;
+import org.example.eatopia.domain.refund.exception.RefundException;
+import org.example.eatopia.domain.refund.repository.RefundRepository;
+import org.example.eatopia.domain.refund.validator.RefundValidator;
+import org.example.eatopia.domain.user.entity.User;
+import org.example.eatopia.domain.user.service.query.UserQueryService;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class RefundCommandServiceImpl implements RefundCommandService {
+
+    private final RefundRepository refundRepository;
+
+    private final UserQueryService userQueryService;
+    private final ProductCommandService productCommandService;
+    private final OrderDetailQueryService orderDetailQueryService;
+    private final PaymentQueryService paymentQueryService;
+
+    private final RefundValidator refundValidator;
+    private final ApplicationEventPublisher eventPublisher;
+
+    @Override
+    public RefundResponse requestRefund(Long userId, RefundCreateRequest request) {
+
+        User user = userQueryService.getUserEntityById(userId);
+        OrderDetail orderDetail = orderDetailQueryService.getOrderDetailEntityById(request.orderDetailId());
+        Payment payment = paymentQueryService.getPaymentEntityByOrder(orderDetail.getOrder());
+
+        refundValidator.validateRefundRequest(user, orderDetail);
+
+        BigDecimal refundAmount = orderDetail.getPrice();
+
+        Refund refund = Refund.create(user, payment, orderDetail, refundAmount, request.reason());
+        refund.updateStatus(RefundStatus.PENDING);
+        Refund savedRefund = refundRepository.save(refund);
+
+        return RefundResponse.from(savedRefund);
+    }
+
+    @Override
+    public RefundResponse successRefund(Long refundId) {
+        Refund refund = refundRepository.findById(refundId)
+                .orElseThrow(() -> new RefundException(RefundErrorCode.REFUND_NOT_FOUND));
+
+        refundValidator.validateRefundStatusPending(refund);
+
+        Payment payment = refund.getPayment();
+        OrderDetail orderDetail = refund.getOrderDetail();
+
+        productCommandService.increaseStock(
+                orderDetail.getProduct().getId(),
+                orderDetail.getQuantity()
+        );
+
+        refund.updateStatus(RefundStatus.SUCCESS);
+
+        eventPublisher.publishEvent(new RefundSuccessEvent(refund));
+
+        return RefundResponse.from(refund);
+    }
+
+    @Override
+    public RefundResponse canceledRefund(Long refundId) {
+        Refund refund = refundRepository.findById(refundId)
+                .orElseThrow(() -> new RefundException(RefundErrorCode.REFUND_NOT_FOUND));
+
+        refundValidator.validateRefundStatusPending(refund);
+
+        refund.updateStatus(RefundStatus.CANCELED);
+
+        return RefundResponse.from(refund);
+    }
+}
