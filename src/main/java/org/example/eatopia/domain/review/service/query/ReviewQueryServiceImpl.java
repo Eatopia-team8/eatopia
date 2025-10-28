bc7e8985067c40e87d9cef4228417a5672ecf0a5
Hyjpark <hyej042@gmail.com>
2025-10-28 16:29:12 +0900

    feat/review: 숨김 상태 리뷰 조회시 예외 반환

--- src/main/java/org/example/eatopia/domain/review/service/query/ReviewQueryServiceImpl.java
+++ src/main/java/org/example/eatopia/domain/review/service/query/ReviewQueryServiceImpl.java
@@ -1,9 +1,12 @@
 package org.example.eatopia.domain.review.service.query;
 
 import lombok.RequiredArgsConstructor;
+import org.example.eatopia.common.core.exception.GlobalException;
 import org.example.eatopia.domain.review.dto.request.ReviewSearchCondition;
 import org.example.eatopia.domain.review.dto.response.ReviewSearchResponse;
 import org.example.eatopia.domain.review.dto.response.ReviewSellerResponse;
+import org.example.eatopia.domain.review.enums.ReviewStatus;
+import org.example.eatopia.domain.review.exception.ReviewErrorCode;
 import org.example.eatopia.domain.review.repository.ReviewReportRepository;
 import org.example.eatopia.domain.review.repository.ReviewRepository;
 import org.springframework.data.domain.Page;
@@ -27,6 +30,10 @@ public class ReviewQueryServiceImpl implements ReviewQueryService {
 
     @Override
     public Page<ReviewSellerResponse> getReviewsBySeller(Long productId, Long sellerId, ReviewSearchCondition condition, Pageable pageable) {
+
+        if (condition.status() == ReviewStatus.HIDDEN) {
+            throw new GlobalException(ReviewErrorCode.REVIEW_HIDDEN);
+        }
 
         return reviewRepository.getReviewsBySeller(productId, sellerId, condition, pageable);
     }
