62f2b3c355745b21862b8375adb86558b949da26
Hyjpark <hyej042@gmail.com>
2025-10-28 15:41:25 +0900

    refactor/review: 필터 함수 분리

--- src/main/java/org/example/eatopia/domain/review/repository/ReviewRepositoryImpl.java
+++ src/main/java/org/example/eatopia/domain/review/repository/ReviewRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -60,25 +61,24 @@ public class ReviewRepositoryImpl implements ReviewRepositoryCustom {
 
     private BooleanBuilder buildFilter(Long productId, ReviewSearchCondition condition) {
 
-        BooleanBuilder builder = new BooleanBuilder();
+        return new BooleanBuilder()
+                .and(productFilter(productId))
+                .and(keywordFilter(condition.keyword()))
+                .and(ratingFilter(condition.rating()))
+                .and(review.content.contains(condition.keyword()))
+                .and(review.status.eq(ReviewStatus.ACTIVE);
+    }
 
-        // 특정 상품에 대한 리뷰
-        builder.and(review.product.id.eq(productId));
+    private BooleanExpression productFilter(Long productId) {
+        return productId != null ? review.product.id.eq(productId) : null;
+    }
 
-        // 활성화 상태인 리뷰만 노출
-        builder.and(review.status.eq(ReviewStatus.ACTIVE));
+    private BooleanExpression keywordFilter(String keyword) {
+        return (keyword != null && !keyword.isEmpty()) ? review.content.contains(keyword) : null;
+    }
 
-        // 내용 검색
-        if (StringUtils.hasText(condition.keyword())) {
-            builder.and(review.content.contains(condition.keyword()));
-        }
-
-        // 별점
-        if (condition.rating() != null) {
-            builder.and(review.rating.eq(condition.rating()));
-        }
-
-        return builder;
+    private BooleanExpression ratingFilter(Integer rating) {
+        return rating != null ? review.rating.eq(rating) : null;
     }
 
     private OrderSpecifier<?> getOrderSpecifier(Sort sort) {
