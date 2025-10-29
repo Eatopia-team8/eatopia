941c2d13bb22753a6d5fa872cd71ccdd70415faa
dbdnsdb123 <dbdnsdb123@naver.com>
2025-10-29 21:24:58 +0900

    feat/payment: swagger 추가

--- src/main/java/org/example/eatopia/domain/payment/controller/PaymentController.java
+++ src/main/java/org/example/eatopia/domain/payment/controller/PaymentController.java
@@ -1,6 +1,9 @@
 package org.example.eatopia.domain.payment.controller;
 
 import com.siot.IamportRestClient.exception.IamportResponseException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.eatopia.common.core.dto.Response;
@@ -24,6 +27,12 @@ public class PaymentController {
 
     private final PaymentCommandService paymentCommandService;
 
+    @Operation(summary = "결제 생성", description = "주문에 대한 결제 정보를 생성합니다.",
+            security = {@SecurityRequirement(name = "bearerAuth")},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "생성 성공"),
+                    @ApiResponse(responseCode = "400", description = "생성 실패")
+            })
     @PreAuthorize("hasRole('ROLE_BUYER')")
     @PostMapping
     public ResponseEntity<PaymentResponse> createPayment(
@@ -36,6 +45,12 @@ public class PaymentController {
         return ResponseEntity.ok(createdPayment);
     }
 
+    @Operation(summary = "결제 수단 변경", description = "결제 수단을 변경합니다.(pending 일때 가능)",
+            security = {@SecurityRequirement(name = "bearerAuth")},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "변경 성공"),
+                    @ApiResponse(responseCode = "400", description = "변경 실패")
+            })
     @PreAuthorize("hasRole('ROLE_BUYER')")
     @PatchMapping("/{paymentId}/method")
     public ResponseEntity<PaymentResponse> updatePaymentMethod(
@@ -47,6 +62,12 @@ public class PaymentController {
         return ResponseEntity.ok(updatedPayment);
     }
 
+    @Operation(summary = "결제 검증", description = "Portone에서 결제 후 검증을 합니다.",
+            security = {@SecurityRequirement(name = "bearerAuth")},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "생성 성공"),
+                    @ApiResponse(responseCode = "400", description = "생성 실패")
+            })
     @PreAuthorize("hasRole('ROLE_BUYER')")
     @PostMapping("/verify")
     public ResponseEntity<Response<PaymentResponse>> verifyPayment(
