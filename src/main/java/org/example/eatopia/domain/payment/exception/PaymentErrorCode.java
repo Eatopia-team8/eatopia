68053da083079d3afa4650431933d4d045747aa2
dbdnsdb123 <dbdnsdb123@naver.com>
2025-10-29 21:19:15 +0900

    feat/payment: error 코드 추가

--- src/main/java/org/example/eatopia/domain/payment/exception/PaymentErrorCode.java
+++ src/main/java/org/example/eatopia/domain/payment/exception/PaymentErrorCode.java
@@ -11,7 +11,7 @@ public enum PaymentErrorCode implements ErrorCode {
 
     PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "PAY-001", "결제 정보를 찾을 수 없습니다."),
     CANNOT_CANCEL_PAYMENT(HttpStatus.BAD_REQUEST, "PAY-002", "결제를 취소할 수 없는 상태입니다."),
-    CANNOT_COMPLETE_PAYMENT(HttpStatus.BAD_REQUEST, "PAY-003", "결제를 완료할 수 없는 상태입니다."),
+    PAYMENT_CANCELED_FAILED(HttpStatus.BAD_REQUEST, "PAY-003", "결제 취소 처리 중 오류가 발생했습니다."),
     ALREADY_PAID_ORDER(HttpStatus.CONFLICT, "PAY-004", "이미 결제가 완료된 주문입니다."),
     CANNOT_UPDATE_METHOD(HttpStatus.BAD_REQUEST, "PAY-005", "결제 수단을 변경할 수 없는 상태입니다."),
 
