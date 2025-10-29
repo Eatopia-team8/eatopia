e5bf027c1b92a9ef90e5eeca99af5f37d133aa94
dbdnsdb123 <dbdnsdb123@naver.com>
2025-10-29 21:18:18 +0900

    feat/payment: enums 생성

--- src/main/java/org/example/eatopia/domain/payment/dto/request/PaymentCreateRequest.java
+++ src/main/java/org/example/eatopia/domain/payment/dto/request/PaymentCreateRequest.java
@@ -1,6 +1,6 @@
 package org.example.eatopia.domain.payment.dto.request;
 
-import org.example.eatopia.domain.payment.entity.PaymentMethod;
+import org.example.eatopia.domain.payment.enums.PaymentMethod;
 
 public record PaymentCreateRequest(
         Long orderId,
