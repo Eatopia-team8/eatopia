45b20e3461d27d6d1d57968ad96b74af5b56be1e
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-10-29 20:03:29 +0900

    refactor/cache: 캐시 변경

--- src/main/java/org/example/eatopia/domain/product/dto/response/ProductListResponse.java
+++ src/main/java/org/example/eatopia/domain/product/dto/response/ProductListResponse.java
@@ -5,15 +5,15 @@ import org.springframework.data.domain.Page;
 import java.util.List;
 
 public record ProductListResponse(
-        List<ProductResponse> products,
+        List<ProductResponse> content,
         int currentPage,
         int totalPages,
         long totalElements,
         int pageSize
 ) {
-    public static ProductListResponse of(List<ProductResponse> products, Page<?> page) {
+    public static ProductListResponse of(List<ProductResponse> content, Page<?> page) {
         return new ProductListResponse(
-                products,
+                content,
                 page.getNumber(),
                 page.getTotalPages(),
                 page.getTotalElements(),
