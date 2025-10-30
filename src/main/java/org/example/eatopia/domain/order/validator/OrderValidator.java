83c9a29a700b617d3b3a63d46ef0db11c2a4b820
dbdnsdb123 <dbdnsdb123@naver.com>
2025-10-30 15:48:27 +0900

    feat/order: 0원, 장바구니 확인 로직 추가

--- src/main/java/org/example/eatopia/domain/order/validator/OrderValidator.java
+++ src/main/java/org/example/eatopia/domain/order/validator/OrderValidator.java
@@ -1,12 +1,16 @@
 package org.example.eatopia.domain.order.validator;
 
 import lombok.RequiredArgsConstructor;
-import org.example.eatopia.common.core.exception.GlobalException;
+import org.example.eatopia.domain.cart.entity.CartItem;
 import org.example.eatopia.domain.order.entity.Order;
-import org.example.eatopia.domain.order.entity.OrderStatus;
+import org.example.eatopia.domain.order.enums.OrderStatus;
 import org.example.eatopia.domain.order.exception.OrderErrorCode;
+import org.example.eatopia.domain.order.exception.OrderException;
 import org.example.eatopia.domain.product.entity.Product;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -14,19 +18,31 @@ public class OrderValidator {
 
     public void orderSuccessValidate(Order order) {
         if (order.getStatus() != OrderStatus.PENDING) {
-            throw new GlobalException(OrderErrorCode.CANNOT_SUCCESS_ORDER);
+            throw new OrderException(OrderErrorCode.CANNOT_SUCCESS_ORDER);
         }
     }
 
     public void orderCancelValidate(Order order) {
         if (order.getStatus() != OrderStatus.SUCCESS) {
-            throw new GlobalException(OrderErrorCode.CANNOT_CANCEL_ORDER);
+            throw new OrderException(OrderErrorCode.CANNOT_CANCEL_ORDER);
         }
     }
 
     public void validateStock(Product product, Integer quantity) {
         if (product.getStock() < quantity) {
-            throw new GlobalException(OrderErrorCode.OUT_OF_STOCK);
+            throw new OrderException(OrderErrorCode.OUT_OF_STOCK);
+        }
+    }
+
+    public void validateCartItems(List<CartItem> cartItems) {
+        if (cartItems == null || cartItems.isEmpty()) {
+            throw new OrderException(OrderErrorCode.EMPTY_CART_ORDER);
+        }
+    }
+
+    public void validateFinalPrice(BigDecimal finalPrice) {
+        if (finalPrice == null || finalPrice.compareTo(BigDecimal.ZERO) < 0) {
+            throw new OrderException(OrderErrorCode.INVALID_FINAL_PRICE);
         }
     }
 }
