a5b1a3e876839df6614057b1c46f2a67f2405d7b
dbdnsdb123 <dbdnsdb123@naver.com>
2025-10-31 09:24:42 +0900

    feat/refund: 상수값 추가

--- src/main/java/org/example/eatopia/common/core/consts/Const.java
+++ src/main/java/org/example/eatopia/common/core/consts/Const.java
@@ -1,5 +1,7 @@
 package org.example.eatopia.common.core.consts;
 
+import java.math.BigDecimal;
+
 public abstract class Const {
 
     public static final String LOGIN_USER = "LOGIN_USER";
@@ -15,4 +17,6 @@ public abstract class Const {
     public static final int CODE_LENGTH = 8;
     public static final java.security.SecureRandom RANDOM = new java.security.SecureRandom();
 
+    // 배달비
+    public static final BigDecimal DEFAULT_DELIVERY_PRICE = new BigDecimal("3000");
 }
