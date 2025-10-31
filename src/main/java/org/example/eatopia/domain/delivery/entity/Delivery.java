1fd636bcfb7b948c63d970a631433468e2a2e927
dbdnsdb123 <dbdnsdb123@naver.com>
2025-10-31 15:04:43 +0900

    feat/delivery: 배달 기본 구조 추가 및 swagger 태그 추가

---
+++ src/main/java/org/example/eatopia/domain/delivery/entity/Delivery.java
@@ -0,0 +1,51 @@
+package org.example.eatopia.domain.delivery.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.eatopia.common.core.entity.BaseEntity;
+import org.example.eatopia.domain.delivery.enums.DeliveryStatus;
+import org.example.eatopia.domain.order.entity.Order;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "deliveries")
+public class Delivery extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false, unique = true)
+    private Order order;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DeliveryStatus status;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Delivery(Order order) {
+        this.order = order;
+        this.status = DeliveryStatus.PREPARING;
+    }
+
+    /**
+     * 배달 엔티티 생성 정적 팩토리 메서드
+     */
+    public static Delivery create(Order order) {
+        return Delivery.builder()
+                .order(order)
+                .build();
+    }
+
+    /**
+     * 배달 상태 업데이트
+     */
+    public void updateStatus(DeliveryStatus status) {
+        this.status = status;
+    }
+}
