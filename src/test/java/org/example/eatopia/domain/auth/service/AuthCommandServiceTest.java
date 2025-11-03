74261e3011d95a1c55df7d692f66045fcf532982
seongmin2223 <khyuno9@naver.com>
2025-11-03 18:18:55 +0900

    test/auth-test: Auth(로그인,로그아웃등등)관련 테스트코드 제미나이 코드리뷰관련 수정

--- src/test/java/org/example/eatopia/domain/auth/service/AuthCommandServiceTest.java
+++ src/test/java/org/example/eatopia/domain/auth/service/AuthCommandServiceTest.java
@@ -26,7 +26,7 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
-class AuthControllerTest {
+class AuthServiceTest {
 
     @Autowired
     private AuthController authController;
