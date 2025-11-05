de1d529ba22d92a8a72ddae3fb966302d69a5b7a
Hyjpark <hyej042@gmail.com>
2025-11-05 20:39:31 +0900

    refactor/cart: 장바구니 아이템 증가 로직의 가독성 개선

--- src/main/java/org/example/eatopia/domain/cart/service/command/CartItemTransactionalService.java
+++ src/main/java/org/example/eatopia/domain/cart/service/command/CartItemTransactionalService.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class CartItemTransactionalService {
@@ -20,18 +22,25 @@ public class CartItemTransactionalService {
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void increaseCartItemQuantity(Cart cart, Product product, int quantity) {
-        CartItem cartItem;
 
-        try {
-            // CartItem 잠금 조회 (비관적 락)
-            cartItem = cartItemRepository.findLockedByCartIdAndProductId(cart.getId(), product.getId())
-                    .orElseGet(() -> cartItemRepository.save(CartItem.create(cart, product, 0)));
-        } catch (DataIntegrityViolationException e) {
-            // 다른 트랜잭션이 동시에 생성했을 경우 재조회
-            cartItem = cartItemRepository.findLockedByCartIdAndProductId(cart.getId(), product.getId())
-                    .orElseThrow(() -> new GlobalException(CartErrorCode.CONCURRENT_MODIFICATION));
+        // CartItem 잠금 조회 (비관적 락)
+        Optional<CartItem> cartItemOptional = cartItemRepository.findLockedByCartIdAndProductId(cart.getId(), product.getId());
+
+        if (cartItemOptional.isPresent()) {
+            // 상품이 있으면 수량 증가
+            CartItem cartItem = cartItemOptional.get();
+            cartItem.addQuantity(quantity);
+        } else {
+            // 상품이 없으면 새로 생성
+            try {
+                CartItem cartItem = CartItem.create(cart, product, quantity);
+                cartItemRepository.save(cartItem);
+            } catch (DataIntegrityViolationException e) {
+                // 다른 트랜잭션이 동시에 생성했을 경우 재조회 후, 수량 증가
+                CartItem cartItem = cartItemRepository.findLockedByCartIdAndProductId(cart.getId(), product.getId())
+                        .orElseThrow(() -> new GlobalException(CartErrorCode.CONCURRENT_MODIFICATION));
+                cartItem.addQuantity(quantity);
+            }
         }
-
-        cartItem.addQuantity(quantity);
     }
 }
