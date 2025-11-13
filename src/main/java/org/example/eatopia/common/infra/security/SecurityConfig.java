8ed9311b24cd2f7c18c764b31b9947633f4b2ac7
seongmin2223 <khyuno9@naver.com>
2025-11-13 14:44:46 +0900

    fix/prod: AWS관련오류

--- src/main/java/org/example/eatopia/common/infra/security/SecurityConfig.java
+++ src/main/java/org/example/eatopia/common/infra/security/SecurityConfig.java
@@ -46,9 +46,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:63342", "http://localhost:8081"
-        ));
+
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
